7cf62fa4fb9b904fef7b7bc96d220750c0172050
Jessica <jpalardyudsen@gmail.com>
2023-11-08 12:27:56 +0100

    Rearranged a little for clarity

--- r/darts/darts.R
+++ r/darts/darts.R
@@ -1,16 +1,16 @@
 score <- function(x, y) {
   rCoord <- sqrt(x**2 + y**2)
-  if(rCoord > 10){
-    return(0)
-  }
   if(rCoord <= 1){
-    return(10)  
+    return(10)
   }
   if(rCoord <= 5){
     return(5)
   }
-  else{
+  if(rCoord <= 10){
     return(1)
+  }
+  else{
+    return(0)
   }
 }
 
